2bb1769c8c0e882b62bf03c6f16d314ded446ac6
becaale <becaale@hotmail.com>
2022-12-03 22:03:35 +0200

    agragado filtrado de resultados

--- src/App.js
+++ src/App.js
@@ -1,34 +1,71 @@
+import React from "react";
 import "./App.css";
+import Nav from "./components/Nav.jsx";
 import Card from "./components/Card.jsx";
 import Cards from "./components/Cards.jsx";
 import styles from "./App.module.css";
-import SearchBar from "./components/SearchBar.jsx";
-import characters, { Rick } from "./data.js";
+import { useState, useEffect } from "react";
+/* import characters, { Rick } from "./data.js";
 
-let uno = [Rick];
+let uno = [Rick]; */
 
 function App() {
+  const [characters, setCharacters] = useState([
+    {
+      id: 0,
+      name: "Morty Smith",
+      species: "Human",
+      gender: "Male",
+      image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    },
+  ]);
+  /* 
+  useEffect(() => {}, []); */
+
+  const onSearch = (character) => {
+    fetch(`https://rickandmortyapi.com/api/character/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters((charsPrev) => {
+          return [...charsPrev, ...filterResults(data.results, character)];
+        });
+      });
+  };
+
+  const filterResults = (results, character) => {
+    let arrResult = [];
+    let arrReturn = [];
+
+    arrResult = [
+      ...results.filter(
+        (element) =>
+          element.name
+            .toString()
+            .toLowerCase()
+            .indexOf(character.name.toString().toLowerCase()) >= 0
+      ),
+    ];
+    arrResult.map((element) => {
+      arrReturn.push({
+        id: element.id,
+        name: element.name,
+        species: element.species,
+        gender: element.gender,
+        image: element.image,
+      });
+    });
+    return arrReturn;
+  };
+
   return (
     <div className="App" style={{ padding: "25px" }}>
-      {/* <div>
-        <Cards characters={uno} /> */}
-      {/*         <Card 
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert("Emulamos que se cierra la card")}
-        /> */}
-      {/* </div>
-      <hr /> */}
       <div>
-        <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+        <Nav onSearch={onSearch} />
       </div>
-            {/* <hr /> */}
+      {/* <hr /> */}
       <div>
         <Cards characters={characters} />
       </div>
-
     </div>
   );
 }
