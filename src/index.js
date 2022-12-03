52533793ac7d0ba28346c6576d58e68df0b21af5
becaale <becaale@hotmail.com>
2022-12-03 22:00:42 +0200

    agragada compatibilidad react 18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+/* import ReactDOM from 'react-dom' */ //se quito por estar deprecated en react 18
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+//se implementa eso para react 18 segun documentacion
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <App />
+  /*   document.getElementById('root') */
+);
